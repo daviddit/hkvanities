594ef8350ed81049e0b8dc869d4057ccf9019cbe
David <david@dieres.com>
2021-03-22 12:47:19 +0100

    (mobile) less spacing between burger bars

--- frontend/components/meta/index.js
+++ frontend/components/meta/index.js
@@ -6,6 +6,7 @@ const Meta = () => (
       <meta charSet="utf-8" />
       <link rel="shortcut icon" href="/static/favicon.ico" />
       <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+      <link rel="stylesheet" type="text/css" href="/static/styles.css" />
       <title>HK Vanities</title>
     </Head>
 
