82eb5fe28e5a917992184d68c3453d205766f41e
David <david@dieres.com>
2021-03-30 13:06:43 +0200

    change url name for production website

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 export const site_name = "HKVANIT1ES"
 export const endpoint = `http://localhost:4444`;
 export const prodEndpoint = ``;
-export const host = `https://hkvanities.dieres.com`;
+export const host = `https://hkvanities.com`;
 export const host_dev = `https://hkvanitiesdev.dieres.com`;
 
 export const hostname = process.env.NODE_ENV === 'development' ? host_dev: host
