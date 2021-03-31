9e8d9cdc9636a50da7d17948faf37a7074509deb
David <david@dieres.com>
2021-03-31 20:02:05 +0200

    comments cleanup

--- frontend/components/poem/index.js
+++ frontend/components/poem/index.js
@@ -6,7 +6,6 @@ import { hostname, site_name } from '../../config.js'
 import Head from 'next/head'
 
 
-    //<StyledPoem autoPlay muted loop id={poem.text} key={poem.video}>
 const Poem = ({ poem, controls, autoPlay, showTitle }) => {
 
  const autoplay = autoPlay ? true : null
@@ -28,35 +27,6 @@ const Poem = ({ poem, controls, autoPlay, showTitle }) => {
 	     e.target.currentTime = 0
 	}
  }
-
-/*
-<meta property="og:title" content="How to change the address bar color in Chrome, Firefox, Opera, Safari" />
-<meta property="og:description" content="How to change the address bar color in Chrome, Firefox, Opera, Safari" />
-<meta property="og:url" content="http://webdevelopmentscripts.com/64-how-to-change-the-address-bar-color-in-chrome-firefox-opera-safari" />
-<meta property="og:image" content="http://webdevelopmentscripts.com/post-images/685b-change-browser-address-bar-color-chrome-android.jpeg" />
-
-
-	<meta property="og:locale" content="ca_ES" />
-	<meta property="og:type" content="article" />
-	<meta property="og:title" content="La CUP confirma que proposa Pau Juvillà per presidir el parlament" />
-	<meta property="og:description" content="Riera diu que JxCat i ERC s&#039;han de pronunciar sobre la seva proposta i veure si hi ha candidats alternatius" />
-	<meta property="og:url" content="https://www.vilaweb.cat/noticies/cup-confirma-pau-juvilla-candidat-presidencia-parlament/" />
-	<meta property="og:site_name" content="VilaWeb" />
-	<meta property="article:modified_time" content="2021-03-10T08:32:42+00:00" />
-	<meta property="og:image" content="https://imatges.vilaweb.cat/nacional/wp-content/uploads/2021/02/h_3759144.jpg" />
-	<meta property="og:image:width" content="1024" />
-	<meta property="og:image:height" content="682" />
-	<meta name="twitter:card" content="summary_large_image" />
-
-	<meta property="og:player" content={url_video} />
-	<meta property="og:player:width" content="300" />
-	<meta property="og:player:height" content="300" />
-	<meta property="og:player:stream" content={url_video} />
-*/
-
-  ////const url = "https://" + 'hkvanities.dieres.com'+ poem.slug;
-  ////const url_img = poem.thumb ? "https://" + 'hkvanities.dieres.com'+ poem.thumb.replace('/ /g','+') : null
-  //const url_video = poem.thumb ? "https://" + 'hkvanities.dieres.com'+ poem.video.replace('/ /g','+') : null
 
   const url = poem.slug ? hostname + poem.slug.replace('/ /g','%2520') : poem.url
   const url_img = hostname + poem.thumb.replace('/ /g','%2520')
