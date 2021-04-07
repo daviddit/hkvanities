d92b0ba3a3699f33407ea95b1791c7f37a2293f6
David <david@dieres.com>
2021-04-07 16:16:47 +0200

    share text and selection text changed

--- frontend/components/selection/index.js
+++ frontend/components/selection/index.js
@@ -90,7 +90,7 @@ const router = useRouter()
             </React.Fragment>
           ) : (
             <StyledPlaceholderDropZone innerRef={drop} active={canDrop}>
-              <StyledSelectionNote>{'Select, move the images and create your own poem, then share or save on your device.'}</StyledSelectionNote>
+              <StyledSelectionNote>{'Select, shuffle the images and create your own poem, then share or save on your device.'}</StyledSelectionNote>
               <StyledDropZone innerRef={drop} active={canDrop}></StyledDropZone>
             </StyledPlaceholderDropZone>
           )}
